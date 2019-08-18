4026db654257d5745a7b115981cb82398bd09612
Fatemeh Pakpour <fatemehpakpour64@gmail.com>
2019-08-18 21:24:50 +0200

    added the diagonal matrix function

---
+++ math/main.js
@@ -0,0 +1,14 @@
+function isDiagonalMatrix(matrix){
+    for(let i = 0; i <matrix.length; i++ ){
+        for(let j = 0; j < matrix.length; j++ ){
+            if(i !== j && matrix[i][j] !== 0){
+                return false
+            }    
+        }    
+    }
+    return true;
+
+}
+console.log(isDiagonalMatrix([[1, 0, 0], [0, 5, 0], [0, 0, 3] ])); 
+console.log(isDiagonalMatrix([[1, 0, 0], [0, 5, 0], [2, 0, 3] ])); 
+
