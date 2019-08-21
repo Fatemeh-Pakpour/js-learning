a67a6b99487729aa07375cff7f50cacf9a38c6e6
Fatemeh Pakpour <fatemehpakpour64@gmail.com>
2019-08-21 11:32:27 +0200

    added a properOrImproper function

--- math/main.js
+++ math/main.js
@@ -27,25 +27,7 @@ for (let i = 0 ; i <= items.length-1; i++) {
 }
 arrayPreviousLess([3, 5, 2, 4, 5]); 
 //  [-1, 3, -1, 2, 4]
-console.log(previousLess); */
-
- let num = 0;
-let i = 2;
-function hailstoneSequence(n) {
-    while(n != 1)
-    {
-    if (n % 2 === 0){
-        num = n;
-    }
-    else{
-        num = 3*n + 1;
-    }
-    return num;
-   
-}
-}
-
-console.log(hailstoneSequence(5)); 
+console.log(previousLess); 
 
 function hailstoneSequence(n) {
   const sequence = [];
@@ -60,7 +42,24 @@ function hailstoneSequence(n) {
     sequence.push(n);
   }
 
-  return sequence.join("=>");
+//   return sequence.join("=>");
+return sequence.length;  
 }
-
 console.log(hailstoneSequence(18));
+
+
+function properOrImproper(a) {
+  for(const item of a ){
+   if (item !== 0 && item >= 10 && item <= -10){
+    return "proper a"
+   }
+  }
+   if ( a[0] / a[1] < 1 && a[0] / a[1] > 0 ){
+    return "Proper"
+   }
+   else{
+    return "Improper"
+   }
+  }
+ 
+  console.log(properOrImproper([-2,3]));
